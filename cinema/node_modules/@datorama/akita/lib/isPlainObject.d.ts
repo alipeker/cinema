75c33c9a9d312b74b1e0fb988b0a995344ab9d5b
1alipeker@gmail.com <1alipeker@gmail.com>
2021-08-18 18:16:12 +0300

    node modules removed

--- cinema/node_modules/@datorama/akita/lib/isPlainObject.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function isPlainObject(value: any): boolean;
--- cinema/node_modules/@stomp/rx-stomp/esm6/rx-stomp-rpc-config.js
+++
@@ -1,6 +0,0 @@
-/**
- * RPC Config. See the guide for example.
- */
-export class RxStompRPCConfig {
-}
-//# sourceMappingURL=rx-stomp-rpc-config.js.map
